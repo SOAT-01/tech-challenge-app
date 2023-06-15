2909b1c532e9f259ff08fad03a445e60644d2a8b
Carol Pinheiro <caroline.bpinheiro@outlook.com>
2023-06-15 08:56:49 -0300

    feat: created instance of produtoController

--- src/adapter/driver/api/controllers/index.ts
+++ src/adapter/driver/api/controllers/index.ts
@@ -1,8 +1,16 @@
 import { SystemUserRepository } from "@infra/repositories/systemUserRepository";
+import { SystemProdutoRepository } from "@infra/repositories/systemProdutoRepository";
 import { SystemUserUseCase } from "@useCases/user";
 import { UserController } from "./userController";
+import { SystemProdutoUseCase } from "@useCases/produto";
+import { ProdutoController } from "./produto/produtoController";
 
 const systemUserRepository = new SystemUserRepository();
 const systemUserUseCase = new SystemUserUseCase(systemUserRepository);
 
 export const userController = new UserController(systemUserUseCase);
+
+const systemProdutoRepository = new SystemProdutoRepository();
+const systemProdutoUseCase = new SystemProdutoUseCase(systemProdutoRepository);
+
+export const produtoController = new ProdutoController(systemProdutoUseCase);
