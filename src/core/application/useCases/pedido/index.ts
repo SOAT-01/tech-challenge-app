16d873af74fdba9881c6152435e67fdca4a3391d
Karine Riquena Limp <karine_limp@hotmail.com>
2023-06-21 21:44:48 -0300

    feat(order): create core application use cases

---
+++ src/core/application/useCases/pedido/index.ts
@@ -0,0 +1,30 @@
+import { IPedidoUseCase } from "./pedido.interface";
+import { Pedido } from "@domain/entities/pedido";
+import { AssertionConcern } from "@domain/base/assertionConcern";
+import { IPedidoRepository } from "@domain/repositories/pedidoRepository.interface";
+
+export class PedidoUseCase implements IPedidoUseCase {
+    private readonly pedidoRepository: IPedidoRepository;
+
+    constructor(produtoRepository: IPedidoRepository) {
+        this.pedidoRepository = produtoRepository;
+    }
+
+    getPedidos(): Promise<Pedido[]> {
+      return this.pedidoRepository.getPedidos();
+    }
+
+    async updatePedido(id: string, pedido: Omit<Partial<Pedido>, "id">): Promise<Pedido> {
+      AssertionConcern.assertArgumentNotEmpty(pedido, "Pedido is required");
+
+      const doesPedidoExists = await this.pedidoRepository.getPedidoById(
+        id,
+      );
+
+      if (!doesPedidoExists) {
+        throw new Error("Pedido não encontrado");
+      }
+
+      return this.pedidoRepository.updatePedido(id, pedido);
+    }
+}
