1d8771fc7d7b25e4d7c4c2824778d6ba05b56d86
Karine Riquena Limp <karine_limp@hotmail.com>
2023-06-21 21:45:57 -0300

    feat(order): create adapter driver api controller

--- src/adapter/driver/api/controllers/index.ts
+++ src/adapter/driver/api/controllers/index.ts
@@ -8,11 +8,19 @@ import { ProdutoUseCase } from "@useCases/produto";
 import { ClienteController } from "./cliente";
 import { ProdutoController } from "./produto";
 
+import { PedidoModel } from "@infra/mongo/models/Pedido";
+import { PedidoMongoRepository } from "@infra/mongo/repositories/pedidoRepository";
+import { PedidoUseCase } from "@useCases/pedido";
+import { PedidoController } from "./pedido/pedidoController";
+
 const clienteRepository = new ClienteMongoRepository(ClienteModel);
 const produtoRepository = new ProdutoMongoRepository(ProdutoModel);
+const pedidoRepository = new PedidoMongoRepository(PedidoModel);
 
 const clienteUseCase = new ClienteUseCase(clienteRepository);
 const produtoUseCase = new ProdutoUseCase(produtoRepository);
+const pedidoUseCase = new PedidoUseCase(pedidoRepository);
 
 export const clienteController = new ClienteController(clienteUseCase);
 export const produtoController = new ProdutoController(produtoUseCase);
+export const pedidoController = new PedidoController(pedidoUseCase);
