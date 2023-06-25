5f5eed05d4f54acb365252b1c67d6eeb791ea9dc
HenriqueMitsuo <henrique@melhorplano.net>
2023-06-25 13:52:22 -0300

    refactor(ClienteRepository): - make email and cpf unique on mongo implementation - update return to match interface - renamed verificarDuplicado to checkDuplicate

--- src/adapter/driven/infra/mongo/repositories/clienteRepository.ts
+++ src/adapter/driven/infra/mongo/repositories/clienteRepository.ts
@@ -1,29 +1,62 @@
 import { Cliente } from "@domain/entities/cliente";
 import { ClienteRepository } from "@domain/repositories/clienteRepository.interface";
 import { ClienteModel } from "../models";
+import { Cpf, Email } from "@domain/valueObjects";
 
 export class ClienteMongoRepository implements ClienteRepository {
     constructor(private readonly clienteModel: typeof ClienteModel) {}
 
-    public create(cliente: Cliente): Promise<Cliente> {
-        return this.clienteModel.create({
+    public async create(cliente: Cliente): Promise<Cliente> {
+        const result = await this.clienteModel.create({
             nome: cliente.nome,
             email: cliente.email.value,
-            cpf: cliente?.cpf?.value,
+            cpf: cliente.cpf.value,
+        });
+
+        return new Cliente({
+            id: result.id,
+            nome: result.nome,
+            cpf: Cpf.create(result.cpf),
+            email: Email.create(result.email),
         });
     }
 
-    public getByCpf(cpf: string): Promise<Cliente> {
-        return this.clienteModel.findOne({
+    public async getByCpf(cpf: string): Promise<Cliente> {
+        const result = await this.clienteModel.findOne({
             cpf: cpf,
             deleted: { $ne: true },
         });
+
+        return new Cliente({
+            id: result.id,
+            nome: result.nome,
+            cpf: Cpf.create(result.cpf),
+            email: Email.create(result.email),
+        });
     }
 
-    public getByEmail(email: string): Promise<Cliente> {
-        return this.clienteModel.findOne({
+    public async getByEmail(email: string): Promise<Cliente> {
+        const result = await this.clienteModel.findOne({
             email: email,
             deleted: { $ne: true },
         });
+
+        return new Cliente({
+            id: result.id,
+            nome: result.nome,
+            cpf: Cpf.create(result.cpf),
+            email: Email.create(result.email),
+        });
+    }
+
+    public async checkDuplicate(args: {
+        email: string;
+        cpf?: string;
+    }): Promise<boolean> {
+        const result = await this.clienteModel.count({
+            $or: [{ cpf: args.cpf, email: args.email }],
+        });
+
+        return result > 0;
     }
 }
