17cfb4150f8ff7d295354403493fc90c4491a3fb
Daniel Eiro <daniel.eiro.01@gmail.com>
2023-06-25 11:04:10 -0300

    feat: add script to test Mongo connection

---
+++ src/adapter/driven/infra/mongo/helpers/testConnection.ts
@@ -0,0 +1,18 @@
+import { serverConfig } from "@config/index";
+import mongoose from "mongoose";
+
+const mongodbConnString = serverConfig.mongo.connectionString;
+
+export default async function testConnection(): Promise<boolean> {
+    try {
+        await mongoose
+            .connect(mongodbConnString, {
+                dbName: serverConfig.mongo.dbName,
+                serverSelectionTimeoutMS: 500
+            });
+        return true;
+    }
+    catch(error) {
+        return false;
+    }
+}
