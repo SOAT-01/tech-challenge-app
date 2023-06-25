908eca2c4aac89e2ae7ed9cc028a99ea082acba6
HenriqueMitsuo <henrique@melhorplano.net>
2023-06-25 13:56:19 -0300

    refactor(ClienteController): - update controller format - add statusCode constant usage - update usaCase usage

--- src/adapter/driver/api/routes/clienteRouter.ts
+++ src/adapter/driver/api/routes/clienteRouter.ts
@@ -7,9 +7,11 @@ export function makeClienteRouter(): Router {
     clienteRouter.post("/", async (req, res) =>
         clienteController.post(req, res),
     );
+
     clienteRouter.get("/by-email/:email", async (req, res) =>
         clienteController.getByEmail(req, res),
     );
+
     clienteRouter.get("/by-cpf/:cpf", async (req, res) =>
         clienteController.getByCpf(req, res),
     );
