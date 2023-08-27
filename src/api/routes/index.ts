dee8850af47bfb6c369b566a38e59b5ec7b25a70
HenriqueMitsuo <henrique@melhorplano.net>
2023-08-27 19:33:55 -0300

    feat(api): add webhook Controller and Router

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -3,6 +3,7 @@ import { makeClienteRouter } from "./clienteRouter";
 import { makeProdutoRouter } from "./produtoRouter";
 import { makePedidoRouter } from "./pedidoRouter";
 import { makeHealthRouter } from "./healthRouter";
+import { makeWebhookRouter } from "./webhookRouter";
 
 export function makeServerRouter(): Router {
     const serverRouter = Router();
@@ -11,6 +12,7 @@ export function makeServerRouter(): Router {
     serverRouter.use("/produto", makeProdutoRouter());
     serverRouter.use("/pedido", makePedidoRouter());
     serverRouter.use("/health", makeHealthRouter());
+    serverRouter.use("/webhook", makeWebhookRouter());
 
     return serverRouter;
 }
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,8 +2,10 @@ import { ProdutoControllerFactory } from "./produto";
 import { ClienteControllerFactory } from "./cliente";
 import { PedidoControllerFactory } from "./pedido";
 import { HealthControllerFactory } from "./health";
+import { WebhookControllerFactory } from "./webhook";
 
 export const produtoController = ProdutoControllerFactory.create();
 export const clienteController = ClienteControllerFactory.create();
 export const pedidoController = PedidoControllerFactory.create();
 export const healthController = HealthControllerFactory.create();
+export const webhookController = WebhookControllerFactory.create();
