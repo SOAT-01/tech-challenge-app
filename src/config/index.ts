325f2bf76fbdce5e048a157b1a8b9f319c1217f3
HenriqueMitsuo <henrique@melhorplano.net>
2023-08-27 19:34:59 -0300

    feat(config): add mercadoPago config

--- src/config/index.ts
+++ src/config/index.ts
@@ -12,4 +12,13 @@ export const serverConfig = {
             "mongodb://localhost:27017",
         ),
     },
+    mercadoPago: {
+        apiUrl: "https://api.mercadopago.com",
+        userId: parseEnvStr("MERCADO_PAGO_USER_ID", "379061219"),
+        pos: parseEnvStr("MERCADO_PAGO_POS", "STORE001POS001"),
+        token: parseEnvStr(
+            "MERCADO_PAGO_TOKEN",
+            "TEST-2732348911740652-082612-1a10554b72bc3f53eab52110003023d0-379061219",
+        ),
+    },
 } as const;
